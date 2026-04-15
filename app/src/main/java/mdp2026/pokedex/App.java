53ac01ea9befa9a74c6ebae1b15877b2d06b8ef7
Lorenzo Pompili <lorenzo.pompili@studenti.unicam.it>
2026-04-15 19:51:40 +0200

    Working final app

--- app/src/main/java/mdp2026/pokedex/App.java
+++ app/src/main/java/mdp2026/pokedex/App.java
@@ -15,40 +15,9 @@ public class App {
 
 
     public static void main(String[] args) {
-
-        //Loading pokemons into a List
-        List<Pokemon> pokemons = null;
-        pokemons = Mapping.getPokemons();
-//        Arrays.fill(discovered, false);
-//
-//        discovered[10] = true;
-
-        //System.out.println(pokemons.size());
-
         boolean exit = true;
         while (exit) {
             Menu.firstMenu();
-            for (Pokemon pokemon : pokemons) {
-                if (pokemon.discovered)
-                   System.out.println(pokemon);
-            }
-            System.out.print("Inserisci un numero per approfondire dettagli sul pokemon o 999 per uscire: ");
-            int choice = sc.nextInt();
-            //1-151
-            if(choice == 999){
-                break;
-            }
-            if (choice > 0 && choice < 152) {
-                if (pokemons.get(choice - 1).discovered) {
-                    pokemons.get(choice - 1).stampaDettagli();
-                }else{
-                    pokemons.get(choice - 1).discovered = true;
-                }
-                //System.out.println(pokemons.get(choice - 1));
-            }else{
-                System.out.println("Inserisci un valore corretto!");
-            }
-
         }
     }
 }
--- app/src/main/java/mdp2026/pokedex/service/Menu.java
+++ app/src/main/java/mdp2026/pokedex/service/Menu.java
@@ -1,13 +1,38 @@
 package mdp2026.pokedex.service;
 
+import mdp2026.pokedex.model.Pokemon;
+
+import java.util.List;
 import java.util.Scanner;
 
 public class Menu {
 
-    Scanner sc = new Scanner(System.in);
+    static Scanner sc = new Scanner(System.in);
+    static List<Pokemon> pokemons = Mapping.getPokemons();
 
     public static void firstMenu(){
-        System.out.println("Benvenuto nel menù principale qui puoi vedere i pokemon scoperti");
+        System.out.println("Inserisci un numero per approfondire dettagli sul pokemon o 999 per uscire: ");
+        for (Pokemon pokemon : pokemons) {
+            if (pokemon.discovered)
+                System.out.println(pokemon);
+        }
+
+        int choice = sc.nextInt();
+        //1-151
+//        if(choice == 999){
+//            break;
+//        }
+        if (choice > 0 && choice < 152) {
+            if (pokemons.get(choice - 1).discovered) {
+                pokemons.get(choice - 1).stampaDettagli();
+            }else{
+                pokemons.get(choice - 1).discovered = true;
+            }
+            //System.out.println(pokemons.get(choice - 1));
+        }else{
+            System.out.println("Inserisci un valore corretto!");
+        }
+
     }
 
     public static int secondMenu(){
